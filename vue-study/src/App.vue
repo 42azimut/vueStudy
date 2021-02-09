09baafcf0f3dc42acaa438f4a1bd5389399d32d9
42azimut <azimutuniverse@gmail.com>
2021-02-09 22:53:50 +0900

    #3 템플릿 문법(v-bind, class)

--- vue-study/src/App.vue
+++ vue-study/src/App.vue
@@ -5,6 +5,10 @@
   <input type="text" v-model="food"/>
   <div v-html="alertMessage"></div>
   <div v-html="subscribeHTML"></div>
+  <img v-bind:src="imageSource" alt="random"/>,
+  <a :href="naverURL">{{ food }}</a>
+  <hr>
+  <h2 v-bind:class="{ red: food==='apple', 'not-good': food==='rice' }">원숭이는 {{ food }} 를 좋아 합니다.</h2>
 </div>
 </template>
 
@@ -16,7 +20,10 @@ export default {
       animal: "Monkey",
       food: "apple",
       alertMessage: "<h2>alert!</h2>",
-      subscribeHTML: `<button onclick="document.querySelector('body').style.background = 'red'">구독하기!!</button>`
+      subscribeHTML: `<button onclick="document.querySelector('body').style.background = 'red'">구독하기!!</button>`,
+      imageSource: "https://placeimg.com/100/100/any",
+      naverURL: "https://www.naver.com",
+
     }
   }
 }
@@ -33,5 +40,18 @@ export default {
 }
 input {
   font-size: 20px;
+}
+
+.orange {
+  color: orange;
+}
+.salmon {
+  color: salmon;
+}
+.red {
+  color: red;
+}
+.not-good {
+  text-decoration: line-through;
 }
 </style>
